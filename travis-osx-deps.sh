62b582bf40e17ed719eb3965d7261432f9c53d72
Adam Caudill <adam@adamcaudill.com>
2014-05-14 13:55:01 -0400

    Try exporting the mono path
    This probably won't work, but giving it a shot anyhow.

--- travis-osx-deps.sh
+++ travis-osx-deps.sh
@@ -8,3 +8,4 @@ brew install cmake
 
 wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg"
 sudo installer -pkg "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
+export MONO_PATH="/Library/Frameworks/Mono.framework/Libraries/mono/4.0/"
