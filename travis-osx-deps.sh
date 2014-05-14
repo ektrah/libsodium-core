c76058fdc360b4f3ca4c083e9bb44b1b613e3fe9
Adam Caudill <adam@adamcaudill.com>
2014-05-14 13:59:43 -0400

    Revert "Try exporting the mono path"
    This reverts commit 62b582bf40e17ed719eb3965d7261432f9c53d72.

--- travis-osx-deps.sh
+++ travis-osx-deps.sh
@@ -8,4 +8,3 @@ brew install cmake
 
 wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg"
 sudo installer -pkg "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
-export MONO_PATH="/Library/Frameworks/Mono.framework/Libraries/mono/4.0/"
