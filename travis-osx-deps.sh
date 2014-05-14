4ef714c095adcaabe3d85bd186879d8bf3aac746
Adam Caudill <adam@adamcaudill.com>
2014-05-14 14:33:42 -0400

    Correct formatting

--- travis-osx-deps.sh
+++ travis-osx-deps.sh
@@ -8,4 +8,4 @@ brew install cmake
 
 wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg"
 sudo installer -pkg "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
-echo 'exec /Library/Frameworks/Mono.framework/Versions/${MONO_VER}/bin/mono --runtime=v4.0 --debug $MONO_OPTIONS /Library/Frameworks/Mono.framework/Versions/${MONO_VER}/lib/mono/2.0/nunit-console.exe "$@"' > nunit-console.sh
+echo "exec /Library/Frameworks/Mono.framework/Versions/${MONO_VER}/bin/mono --runtime=v4.0 --debug $MONO_OPTIONS /Library/Frameworks/Mono.framework/Versions/${MONO_VER}/lib/mono/2.0/nunit-console.exe \"\$@\"" > nunit-console.sh
