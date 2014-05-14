c543184cc7ca21d0042988537ed5be3a862b1311
Adam Caudill <adam@adamcaudill.com>
2014-05-14 14:53:10 -0400

    Update to a newer NUnit for OSX

--- travis-osx-deps.sh
+++ travis-osx-deps.sh
@@ -2,10 +2,14 @@
 set -ev
 
 MONO_VER=3.4.0
+NUNIT_VER=2.6.3
 
 brew update
 brew install cmake
 
 wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg"
 sudo installer -pkg "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
-echo "exec /Library/Frameworks/Mono.framework/Versions/${MONO_VER}/bin/mono --runtime=v4.0 --debug $MONO_OPTIONS /Library/Frameworks/Mono.framework/Versions/${MONO_VER}/lib/mono/2.0/nunit-console.exe \"\$@\"" > nunit-console.sh
+
+wget -O nunit.zip "http://launchpad.net/nunitv2/trunk/${NUNIT_VER}/+download/NUnit-${NUNIT_VER}.zip"
+unzip nunit.zip
+echo "mono --runtime=v4.0  ./NUnit-${NUNIT_VER}/bin/nunit-console.exe \"\$@\"" > nunit-console.sh
