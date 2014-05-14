a5606d01590b0de4371410101e3200d727ab7b2f
Adam Caudill <adam@adamcaudill.com>
2014-05-14 15:45:43 -0400

    Force x86 on OSX.

--- travis-build-libsodium.sh
+++ travis-build-libsodium.sh
@@ -5,6 +5,7 @@ git clone git://github.com/jedisct1/libsodium.git
 cd libsodium
 git checkout tags/0.5.0
 ./autogen.sh
-./configure
+if [[ $TRAVIS_OS_UNAME = 'Linux' ]]; then ./configure; fi
+if [[ $TRAVIS_OS_UNAME = 'Darwin' ]]; then ./configure LDFLAGS="-arch i386"; fi
 make && sudo make install
 if [[ $TRAVIS_OS_UNAME = 'Linux' ]]; then sudo ldconfig; fi
