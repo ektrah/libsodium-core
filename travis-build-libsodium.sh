def249888f54cc3bec7d1c05e170285a1cb2e979
Adam Caudill <adam@adamcaudill.com>
2014-05-14 21:03:04 -0400

    Ok, try x86 for OSX again

--- travis-build-libsodium.sh
+++ travis-build-libsodium.sh
@@ -5,7 +5,10 @@ git clone git://github.com/jedisct1/libsodium.git
 cd libsodium
 git checkout tags/0.5.0
 ./autogen.sh
-if [[ $TRAVIS_OS_UNAME = 'Linux' ]]; then ./configure; fi
-if [[ $TRAVIS_OS_UNAME = 'Darwin' ]]; then ./configure LDFLAGS="-arch i386"; fi
+if [[ $TRAVIS_OS_UNAME = 'Darwin' ]]; then 
+  export CFLAGS="-Os -m32 -arch i386";
+  export LDFLAGS="-m32 -arch i386";
+fi
+./configure
 make && sudo make install
 if [[ $TRAVIS_OS_UNAME = 'Linux' ]]; then sudo ldconfig; fi
