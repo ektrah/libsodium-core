1f7b2ac2283be62b83de292202af275a42a4b776
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-07-04 22:54:30 -0400

    Getting better version of mono

--- travis-linux-deps.sh
+++ travis-linux-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
 
-sudo apt-get install mono-complete
+sudo add-apt-repository ppa:directhex/monoxide && sudo apt-get update
+sudo apt-get install mono-devel
 mozroots --import --sync
 mono --runtime=v4.0 ./.nuget/NuGet.exe restore ./libsodium-net.sln
 echo 'mono --runtime=v4.0 ./packages/NUnit.Runners.2.6.3/tools/nunit-console.exe "$@"' > nunit-console.sh
