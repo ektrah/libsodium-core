f8ab5354763095a002cb40624f1d474d9f791ba8
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-07-04 22:49:10 -0400

    Importing certificates

--- travis-linux-deps.sh
+++ travis-linux-deps.sh
@@ -2,5 +2,6 @@
 set -ev
 
 sudo apt-get install mono-complete
+mozroots --import --sync
 mono --runtime=v4.0 ./.nuget/NuGet.exe restore ./libsodium-net.sln
 echo 'mono --runtime=v4.0 ./packages/NUnit.Runners.2.6.3/tools/nunit-console.exe "$@"' > nunit-console.sh
