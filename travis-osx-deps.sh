d0dab867b36b10213e76150aa8ce3d92b3616b7e
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-07-05 10:33:48 -0400

    mozroots to be called after mono installation

--- travis-osx-deps.sh
+++ travis-osx-deps.sh
@@ -5,9 +5,9 @@ MONO_VER=3.4.0
 
 brew update
 brew install cmake
-mozroots --import --sync
 wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg"
 sudo installer -pkg "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
 
+mozroots --import --sync
 mono --runtime=v4.0 ./.nuget/NuGet.exe restore ./libsodium-net.sln
 echo "mono --runtime=v4.0  ./packages/NUnit.Runners.2.6.3/tools/nunit-console.exe \"\$@\"" > nunit-console.sh
