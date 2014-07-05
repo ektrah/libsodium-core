63299ef76879f25faeb73cecb653df43c58d2a97
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-07-04 22:29:04 -0400

    NUnit 2.6.3 for linux too

--- travis-linux-deps.sh
+++ travis-linux-deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 
-sudo apt-get install mono-devel mono-gmcs nunit-console
-echo 'nunit-console "$@"' > nunit-console.sh
+sudo apt-get install mono-devel mono-gmcs
+mono ./.nuget/NuGet.exe restore ./libsodium-net.sln
+echo 'mono ./packages/NUnit.Runners.2.6.3/tools/nunit-console.exe "$@"' > nunit-console.sh
