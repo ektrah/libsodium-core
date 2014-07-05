d644fb5f0797c0ad22f48dcbf02697ab3d9d8e38
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-07-04 23:01:57 -0400

    Adds command switches

--- travis-linux-deps.sh
+++ travis-linux-deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 
-sudo add-apt-repository ppa:directhex/monoxide && sudo apt-get update
-sudo apt-get install mono-devel
+sudo add-apt-repository ppa:directhex/monoxide -y && sudo apt-get update -qq -y
+sudo apt-get install mono-devel -qq -y
 mozroots --import --sync
 mono --runtime=v4.0 ./.nuget/NuGet.exe restore ./libsodium-net.sln
 echo 'mono --runtime=v4.0 ./packages/NUnit.Runners.2.6.3/tools/nunit-console.exe "$@"' > nunit-console.sh
