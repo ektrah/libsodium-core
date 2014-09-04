23ecdf5840a1f2063c0504c4fb0c0d94c4328b44
Christian Hermann <c.hermann@bitbeans.de>
2014-09-04 15:06:23 +0200

    Added HMAC functions.
    Fixes #20.
    - Added VerifyHmacSha256 (crypto_auth_hmacsha256_verify)
    - Added VerifyHmacSha512 (crypto_auth_hmacsha512_verify)
    - Added SignHmacSha256 (crypto_auth_hmacsha256)
    - Added SignHmacSha512 (crypto_auth_hmacsha512)
    
    Also added some simple tests.

--- Tests/SecretKeyAuthTest.cs
+++ Tests/SecretKeyAuthTest.cs
@@ -31,6 +31,28 @@ namespace Tests
     }
 
     /// <summary>
+    /// Does SecretKeyAuth.SignHmacSha256() return the expected value?
+    /// </summary>
+    [Test]
+    public void SimpleAuthHmacSha256Test()
+    {
+        var expected = Utilities.HexToBinary("1cc0012cfd200becfce64bba779025d02cb349d203e15d44a308e4249e2b7245");
+        var actual = SecretKeyAuth.SignHmacSha256(Encoding.UTF8.GetBytes("Adam Caudill"), Encoding.UTF8.GetBytes("01234567890123456789012345678901"));
+        CollectionAssert.AreEqual(expected, actual);
+    }
+
+    /// <summary>
+    /// Does SecretKeyAuth.SignHmacSha512() return the expected value?
+    /// </summary>
+    [Test]
+    public void SimpleAuthHmacSha512Test()
+    {
+        var expected = Utilities.HexToBinary("9f44681a662b7cde80c4eb34db5102b62a8b482272e3cceef73a334ec1d321c06a99b828e2ff921b4d1304bbd9480adfacf8c4c2ffbcbb4e5663446fda1235d2");
+        var actual = SecretKeyAuth.SignHmacSha512(Encoding.UTF8.GetBytes("Adam Caudill"), Encoding.UTF8.GetBytes("01234567890123456789012345678901"));
+        CollectionAssert.AreEqual(expected, actual);
+    }
+
+    /// <summary>
     /// Does SecretKeyAuth.Verify() return the expected value?
     /// </summary>
     [Test]
@@ -40,6 +62,30 @@ namespace Tests
         Utilities.HexToBinary("9f44681a662b7cde80c4eb34db5102b62a8b482272e3cceef73a334ec1d321c0"),
         Encoding.UTF8.GetBytes("01234567890123456789012345678901"));
       Assert.AreEqual(true, actual);
+    }
+
+    /// <summary>
+    /// Does SecretKeyAuth.VerifyHmacSha256() return the expected value?
+    /// </summary>
+    [Test]
+    public void SimpleVerifyHmacSha256Test()
+    {
+        var actual = SecretKeyAuth.VerifyHmacSha256(Encoding.UTF8.GetBytes("Adam Caudill"),
+          Utilities.HexToBinary("1cc0012cfd200becfce64bba779025d02cb349d203e15d44a308e4249e2b7245"),
+          Encoding.UTF8.GetBytes("01234567890123456789012345678901"));
+        Assert.AreEqual(true, actual);
+    }
+
+    /// <summary>
+    /// Does SecretKeyAuth.VerifyHmacSha512() return the expected value?
+    /// </summary>
+    [Test]
+    public void SimpleVerifyHmacSha512Test()
+    {
+        var actual = SecretKeyAuth.VerifyHmacSha512(Encoding.UTF8.GetBytes("Adam Caudill"),
+          Utilities.HexToBinary("9f44681a662b7cde80c4eb34db5102b62a8b482272e3cceef73a334ec1d321c06a99b828e2ff921b4d1304bbd9480adfacf8c4c2ffbcbb4e5663446fda1235d2"),
+          Encoding.UTF8.GetBytes("01234567890123456789012345678901"));
+        Assert.AreEqual(true, actual);
     }
   }
 }
