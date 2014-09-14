9befc2b994a90d6aa4996b884e714dddb88641ab
Adam Caudill <adam@adamcaudill.com>
2014-09-14 03:47:29 -0400

    Clear the private key on dispose/finalize

--- libsodium-net/KeyPair.cs
+++ libsodium-net/KeyPair.cs
@@ -1,27 +1,44 @@
-﻿namespace Sodium
+﻿using System;
+
+namespace Sodium
 {
   /// <summary>A public / private key pair.</summary>
-  public class KeyPair
+  public class KeyPair : IDisposable
   {
-    /// <summary>Initializes a new instance of the <see cref="KeyPair"/> class.</summary>
-    public KeyPair()
-    {
-      //do nothing
-    }
+    private readonly byte[] _publicKey;
+    private readonly byte[] _privateKey;
 
     /// <summary>Initializes a new instance of the <see cref="KeyPair"/> class.</summary>
     /// <param name="publicKey">The public key.</param>
     /// <param name="privateKey">The private key.</param>
     public KeyPair(byte[] publicKey, byte[] privateKey)
     {
-      PublicKey = publicKey;
-      PrivateKey = privateKey;
+      _publicKey = publicKey;
+      _privateKey = privateKey;
+    }
+
+    ~KeyPair()
+    {
+      Dispose();
     }
 
     /// <summary>Gets or sets the Public Key.</summary>
-    public byte[] PublicKey { get; set; }
+    public byte[] PublicKey
+    {
+      get { return _publicKey; }
+    }
 
     /// <summary>Gets or sets the Private Key.</summary>
-    public byte[] PrivateKey { get; set; }
+    public byte[] PrivateKey
+    {
+      get { return _privateKey; }
+    }
+
+    /// <summary>Dispose of private key in memory.</summary>
+    public void Dispose()
+    {
+      if (_privateKey != null && _privateKey.Length > 0)
+        Array.Clear(_privateKey, 0, _privateKey.Length);
+    }
   }
 }
