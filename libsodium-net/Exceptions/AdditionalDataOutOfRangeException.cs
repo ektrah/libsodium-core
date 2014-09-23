2fa3c828ab25c1211593434f02a10303f9ad9e98
Christian Hermann <c.hermann@bitbeans.de>
2014-09-23 21:27:07 +0200

    Authenticated Encryption with Additional Data
    - Added crypto_aead_chacha20poly1305_encrypt
    - Added crypto_aead_chacha20poly1305_decrypt
    - Added new Exception AdditionalDataOutOfRange
    - Added related tests

---
+++ libsodium-net/Exceptions/AdditionalDataOutOfRangeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class AdditionalDataOutOfRangeException : ArgumentOutOfRangeException
+{
+    public AdditionalDataOutOfRangeException()
+    {
+    }
+
+    public AdditionalDataOutOfRangeException(string message)
+        : base(message)
+    {
+    }
+
+    public AdditionalDataOutOfRangeException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+
+    public AdditionalDataOutOfRangeException(string paramName, object actualValue, string message)
+        : base(paramName, actualValue, message)
+    {
+    }
+}
