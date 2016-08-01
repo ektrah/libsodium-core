bcdc586d59f191495090d612cb44a42f4e3dabaf
Christian Hermann <c.hermann@bitbeans.de>
2016-08-01 11:03:55 +0200

    Update to libsodium 1.0.11 (#145)
    * Update to libsodium 1.0.11
    
    - Fixes #144
    
    * Fix build

--- travis-build-libsodium.sh
+++ travis-build-libsodium.sh
@@ -3,7 +3,7 @@ set -ev
 
 git clone git://github.com/jedisct1/libsodium.git
 cd libsodium
-git checkout tags/1.0.10
+git checkout tags/1.0.11
 ./autogen.sh
 if [[ $TRAVIS_OS_UNAME = 'Darwin' ]]; then 
   export CFLAGS="-Os -m32 -arch i386";
