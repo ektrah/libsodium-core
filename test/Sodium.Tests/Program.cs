52938bc6606049dff0e349227b89c337af7564c0
Raif Atef <beliskner.github.3psil0N@neverbox.com>
2017-08-14 10:34:04 +0200

    Clean up. Remove runtimes, use libsodium package. Make unit test project work.

---
+++ test/Sodium.Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using NUnitLite;
+
+namespace Sodium.Tests
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args);
+    }
+  }
+}
