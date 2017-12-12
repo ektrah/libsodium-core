63ace24deeecb90fb0ec0b1523564d085ecaea5f
Trond Arne Bråthen <tabrath@gmail.com>
2017-12-12 10:36:39 +0100

    Upgrade all
    * All dependencies updated to latest
    * No more NET 4.6.1, only .NET Standard 2.0

--- test/Sodium.Tests/VersionTest.cs
+++ test/Sodium.Tests/VersionTest.cs
@@ -11,7 +11,7 @@ namespace Tests
     [Test]
     public void SodiumVersionStringTest()
     {
-      const string EXPECTED = "1.0.12";
+      const string EXPECTED = "1.0.15";
       var actual = SodiumCore.SodiumVersionString();
       Assert.AreEqual(EXPECTED, actual);
     }
