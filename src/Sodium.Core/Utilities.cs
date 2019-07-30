a7b4e635427758254225f358a686e2b1c85287e7
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-07-30 18:00:38 +0300

    add comments

--- src/Sodium.Core/Utilities.cs
+++ src/Sodium.Core/Utilities.cs
@@ -30,11 +30,16 @@ namespace Sodium
       Upper
     }
 
+    /// <summary>Represents Base64 encoding variants.</summary>
     public enum Base64Variant
     {
+      /// <summary>Original Base64 encoding variant.</summary>
       Original = 1,
+      /// <summary>Original Base64 encoding variant with no padding.</summary>
       OriginalNoPadding = 3,
+      /// <summary>Urlsafe Base64 encoding variant.</summary>
       UrlSafe = 5,
+      /// <summary>Urlsafe Base64 encoding variant with no padding.</summary>
       UrlSafeNoPadding = 7
     }
 
@@ -146,6 +151,11 @@ namespace Sodium
       return arr;
     }
 
+    /// <summary>Takes byte array and converts it to Base64 encoded string.</summary>
+    /// <param name="data">Data to be encoded.</param>
+    /// <param name="variant">Base64 encoding variant.</param>
+    /// <exception cref="OverflowException"></exception>
+    /// <returns>Base64 encoded string.</returns>
     public static string BinaryToBase64(byte[] data, Base64Variant variant = Base64Variant.Original)
     {
       int base64MaxLen = SodiumLibrary.sodium_base64_encoded_len(data.Length, (int)variant);
@@ -159,6 +169,12 @@ namespace Sodium
       return Marshal.PtrToStringAnsi(base64);
     }
 
+    /// <summary>Converts Base64 encoded string to byte array.</summary>
+    /// <param name="base64">Base64 encoded string.</param>
+    /// <param name="ignoredChars">Characters which will be ignored in decoding.</param>
+    /// <param name="variant">Base64 encoding variant</param>
+    /// <exception cref="Exception"></exception>
+    /// <returns>A byte array of decoded Base64 string</returns>
     public static byte[] Base64ToBinary(string base64, string ignoredChars, Base64Variant variant = Base64Variant.Original)
     {
       
