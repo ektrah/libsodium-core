f2ba69b773f098f7b320e709d654de6ee205db2e
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-08-21 13:37:18 +0300

    add test

--- test/Sodium.Tests/PublicKeyAuthTests.cs
+++ test/Sodium.Tests/PublicKeyAuthTests.cs
@@ -1,6 +1,6 @@
-﻿using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Sodium;
+using System.Text;
 
 namespace Tests
 {
@@ -19,6 +19,15 @@ namespace Tests
       Assert.IsNotNull(actual.PublicKey);
     }
 
+    [Test]
+    public void GenerateKeyTesttest()
+    {
+      var actual = PublicKeyAuth.GenerateKeyPair();
+      byte[] randomArray = SodiumCore.GetRandomBytes(255);
+      var sign = PublicKeyAuth.SignDetached(randomArray, actual.PrivateKey);
+      Assert.IsTrue(PublicKeyAuth.VerifyDetached(sign, randomArray, actual.PublicKey));
+    }
+
     /// <summary>Does PublicKeyAuth.GenerateKeyPair(seed) return the expected value?</summary>
     [Test]
     public void GenerateKeySeedTest()
@@ -30,7 +39,7 @@ namespace Tests
       CollectionAssert.AreEqual(expected.PublicKey, actual.PublicKey);
       CollectionAssert.AreEqual(expected.PrivateKey, actual.PrivateKey);
     }
-    
+
     /// <summary>Does PublicKeyAuth.Sign() return the expected value?</summary>
     [Test]
     public void SimpleAuthTest()
@@ -67,9 +76,9 @@ namespace Tests
     {
       var actual = PublicKeyAuth.VerifyDetached(
         Utilities.HexToBinary("8d5436accbe258a6b252c1140f38d7b8dc6196619945818b72512b6a8019d86dfeeb56f40c4d4b983d97dfeed37948527256c3567d6b253757fcfb32bef56f0b"),
-        Encoding.UTF8.GetBytes("Adam Caudill"), 
+        Encoding.UTF8.GetBytes("Adam Caudill"),
         Utilities.HexToBinary("4ffda13c11d61d2b9568e54bec06ea59368e84874883087645e64e5e9653422e"));
-       
+
       Assert.IsTrue(actual);
     }
 
@@ -119,7 +128,7 @@ namespace Tests
       Assert.AreEqual(Utilities.BinaryToHex(curve25519Sk, Utilities.HexFormat.None, Utilities.HexCase.Upper),
                       "8052030376D47112BE7F73ED7A019293DD12AD910B654455798B4667D73DE166");
 
-      for(var i = 0; i < 500; i++)
+      for (var i = 0; i < 500; i++)
       {
         keys = PublicKeyAuth.GenerateKeyPair();
         ed25519Pk = keys.PublicKey;
