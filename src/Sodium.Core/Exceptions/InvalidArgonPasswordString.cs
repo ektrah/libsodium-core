75f6eb65c77a3206864973e3f55883b25ac3cdbc
ektrah <ektrah@users.noreply.github.com>
2022-04-02 00:47:31 +0200

    Add missing accessibility modifier

--- src/Sodium.Core/Exceptions/InvalidArgonPasswordString.cs
+++ src/Sodium.Core/Exceptions/InvalidArgonPasswordString.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Sodium.Exceptions
 {
-    class InvalidArgonPasswordString : Exception
+    internal class InvalidArgonPasswordString : Exception
     {
         public InvalidArgonPasswordString()
           : base("Invalid Password string for Argon 2")
