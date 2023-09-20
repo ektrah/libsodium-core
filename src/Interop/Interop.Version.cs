6dcb273834a004c44f11f16fa3a8b6e01fba69df
bfren <bf@bfren.dev>
2023-09-20 13:12:22 +0100

    Updating libsodium version to 1.0.19

--- src/Interop/Interop.Version.cs
+++ src/Interop/Interop.Version.cs
@@ -7,7 +7,7 @@ internal static partial class Interop
     {
         internal const int SODIUM_LIBRARY_VERSION_MAJOR = 10;
         internal const int SODIUM_LIBRARY_VERSION_MINOR = 3;
-        internal const string SODIUM_VERSION_STRING = "1.0.18";
+        internal const string SODIUM_VERSION_STRING = "1.0.19";
 
         [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int sodium_library_version_major();
