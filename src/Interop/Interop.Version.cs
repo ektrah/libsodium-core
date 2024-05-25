1d4163bfab086d941dbbaa9fdfe5d4dfe7d3dbdc
ektrah <ektrah@users.noreply.github.com>
2024-05-25 20:46:47 +0200

    Update libsodium version to 1.0.20

--- src/Interop/Interop.Version.cs
+++ src/Interop/Interop.Version.cs
@@ -6,8 +6,8 @@ internal static partial class Interop
     internal static partial class Libsodium
     {
         internal const int SODIUM_LIBRARY_VERSION_MAJOR = 26;
-        internal const int SODIUM_LIBRARY_VERSION_MINOR = 1;
-        internal const string SODIUM_VERSION_STRING = "1.0.19";
+        internal const int SODIUM_LIBRARY_VERSION_MINOR = 2;
+        internal const string SODIUM_VERSION_STRING = "1.0.20";
 
         [DllImport(Libraries.Libsodium, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int sodium_library_version_major();
